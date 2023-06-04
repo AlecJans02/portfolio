75dd362f70e0b11ac354f4ef52577bce58ee413e
AlecJans02 <aljans122@gmail.com>
2023-06-04 21:14:12 +1000

    config setting changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
-export default {
-    // other configuration options
-    build: {
-      rollupOptions: {
-        external: ['three']
-      }
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './',
+  build: {
+    rollupOptions: {
+      external: ['three']
     }
   }
+});
+
+
+// export default {
+//     // other configuration options
+//     build: {
+//       rollupOptions: {
+//         external: ['three']
+//       }
+//     }
+//   }
